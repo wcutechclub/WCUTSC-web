0ea766ca9b22360be295ee102ecc27da2ec97de1
Samuel-Tefera <samuelteferab@gmail.com>
2024-10-26 08:58:25 +0300

    make slide fucntional

--- js/script.js
+++ js/script.js
@@ -67,25 +67,25 @@ const totalMembers = document.querySelector('.total-members__num')
 const depNum = document.querySelector('.dep-num')
 const projNum = document.querySelector( '.proj-num' )
 
-const animaCount = function ( targetId, targetValue ) {
+const animaCount = function ( targetId, targetValue, time ) {
   let currentValue = 0;
   const UpdateInterval = setInterval( () => {
-    currentValue += 1;
+    currentValue += 3;
     if ( currentValue >= targetValue ) {
       currentValue = targetValue;
       clearInterval( UpdateInterval );
     }
     targetId.textContent = currentValue;
-  }, 50 )
+  }, time )
 }
 
 const increasNums = function ( entries ) {
     const [ entry ] = entries;
   if ( entry.isIntersecting ) {
-    animaCount(newMembers, 26)
-    animaCount(totalMembers, 42)
-    animaCount(depNum, 4)
-    animaCount(projNum, 10)
+    animaCount(newMembers, 26, 100)
+    animaCount(totalMembers, 42, 75)
+    animaCount(depNum, 4, 500)
+    animaCount(projNum, 10, 300)
   }
 }
 
@@ -95,3 +95,84 @@ const overviewObserver = new IntersectionObserver( increasNums, {
 } );
 
 overviewObserver.observe( overviewSection );
+
+
+// Slider
+const slides = document.querySelectorAll( '.slide' )
+const slider = document.querySelector( '.slider' );
+const btnLeft = document.querySelector( '.slider__btn--left' );
+const btnRight = document.querySelector( '.slider__btn--right' );
+const dotContainer = document.querySelector( '.dots' );
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+
+const creatDots = function () {
+  slides.forEach( ( _, i ) => {
+    dotContainer.insertAdjacentHTML( 'beforeend',
+      `<button class="dots__dot" data-slide="${ i }"></button>` );
+  })
+}
+
+const activateDots = function (slide) {
+  document.querySelectorAll( '.dots__dot' ).forEach( dot =>
+    dot.classList.remove('dots__dot--active'));
+  document.querySelector( `.dots__dot[data-slide="${ slide }"]` )
+    .classList.add( 'dots__dot--active' );
+}
+
+const goToSlide = function (s) {
+  slides.forEach( (slide, index) => {
+    slide.style.transform = `translateX(${ ( ( index - s ) * 100 ) }%)`;
+} )
+}
+
+const nextSlide = function () {
+  if ( curSlide === maxSlide - 1){
+    curSlide = 0;
+  }
+  else {
+    curSlide++;
+  }
+  goToSlide( curSlide );
+  activateDots( curSlide );
+}
+
+const prevSlide = function () {
+  if ( curSlide === 0 ) {
+    curSlide = maxSlide - 1;
+  }
+  else {
+    curSlide--;
+  }
+  goToSlide( curSlide );
+  activateDots( curSlide );
+}
+
+creatDots();
+goToSlide( 0 );
+activateDots( 0 );
+
+// slider by left and right button
+btnRight.addEventListener( 'click', nextSlide );
+btnLeft.addEventListener( 'click', prevSlide );
+
+// Slider by KeyBoard right and left Arrow keys
+document.addEventListener( 'keydown', function ( e ) {
+  if ( e.key === 'ArrowRight' ) {
+    nextSlide();
+  }
+  else if ( e.key === 'ArrowLeft' ) {
+    prevSlide();
+  }
+} );
+
+// Slider by Dots;
+dotContainer.addEventListener( 'click', function(e){
+  if ( e.target.classList.contains( 'dots__dot' ) ) {
+    const slide = e.target.dataset.slide;
+    goToSlide( slide );
+    activateDots( slide );
+  }
+})
